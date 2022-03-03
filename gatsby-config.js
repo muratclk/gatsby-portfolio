3746d19d89275e17ea47af30993c88135ce3d051
muratclk <cashkl@yandex.com>
2022-03-03 06:56:14 +0300

    added gtag for site

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,29 @@ module.exports = {
         openAnalyzer: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-5D3QVVJBGZ", // Google Analytics / GA
+          
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: false,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", ],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.muratcelik.me",
+        },
+      },
+    },
   ].filter(Boolean),
 }
